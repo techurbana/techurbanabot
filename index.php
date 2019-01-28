958b5873a426c6dbb369c7cfee7bbfbbbf743602
techurbana <techurbana@users.noreply.github.com>
2019-01-28 12:53:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,22 +6,8 @@
 
 <body>
 
-<?php echo "Hello Worldddddd!"; ?>
-<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+<?php echo "Hellooouuu Worldddddd!"; ?>
 
-<?php $links = [];
-  foreach($_ENV as $key => $value) {
-    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
-      $links[] = [
-        "name"  => $matches[1],
-        "port"  => $matches[2],
-        "proto" => $matches[3],
-        "value" => $value
-      ];
-    }
-  }
-}
-?>
 
 </body>
 </html>
