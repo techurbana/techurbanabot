7aee8b687c4580470fb0be3f3bdadc9b82bab868
techurbana <techurbana@users.noreply.github.com>
2019-01-28 11:50:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+<title>Hello World!</title>
+</head>
+
+<body>
+
+<?php echo "Hello Worldddddd!"; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+
+<?php $links = [];
+  foreach($_ENV as $key => $value) {
+    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
+      $links[] = [
+        "name"  => $matches[1],
+        "port"  => $matches[2],
+        "proto" => $matches[3],
+        "value" => $value
+      ];
+    }
+  }
+}
+?>
+
+</body>
+</html>
