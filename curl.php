91327188b1e3aab9297970c8632a26537a200529
techurbana <techurbana@users.noreply.github.com>
2019-02-02 13:42:25 +0100

    Create curl.php

---
+++ curl.php
@@ -0,0 +1,20 @@
+<?php
+
+
+function sendMessage($url) {
+    
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
+echo sendMessage("https://www.google.com");
+
+?>
